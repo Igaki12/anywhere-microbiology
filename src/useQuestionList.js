e997c1c348672014dd69f68cfe5d708a8fd8f309
Igaki12 <91039412+Igaki12@users.noreply.github.com>
2023-05-10 12:26:36 +0900

    added terms

--- src/useQuestionList.js
+++ src/useQuestionList.js
@@ -33,7 +33,7 @@ export const useQuestionList = () => {
         //   answerImg: [],
         //   answer: '',
         //   commentary: '',
-        // },
+        // },F
         // {
         //   detailInfo: '',
         //   questionImg: [],
