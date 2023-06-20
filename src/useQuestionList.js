e3aa3e0c22c6801b471ac5465ea329f57f4bf95b
Igaki12 <91039412+Igaki12@users.noreply.github.com>
2023-06-21 08:35:54 +0900

    added q

--- src/useQuestionList.js
+++ src/useQuestionList.js
@@ -1,6 +1,13 @@
 import {
   useState
 } from 'react'
+import tuberculosis from './img/answer/tuberculosis.png';
+import GramMethod from './img/answer/GramMethod.png';
+import disinfection from './img/answer/disinfection.png';
+import cilia from './img/answer/cilia.png';
+import Chlamydiae from './img/answer/Chlamydiae.png';
+import bacteriaStructure from './img/answer/bacteriaStructure.png';
+import antiStain from './img/answer/antiStain.png';
 
 // import from './img/answer/.png'
 
@@ -108,110 +115,99 @@ export const useQuestionList = () => {
         //   commentary: '',
         // },
       ],
-    },{
-      groupTag:"2021 TBL",
-      groupContents:[
-                {
+    }, {
+      groupTag: "2021 TBL",
+      groupContents: [{
           detailInfo: '',
           questionImg: [],
-          questionSentence:
-            '芽胞を形成する菌に有効でない滅菌・消毒法はどれか',
-          answerImg: [],
-          choices:["酸化エチレンガス滅菌","アルコール","高圧蒸気滅菌","過酸化水素低温ガスプラズマ滅菌","界面活性剤"],
-          answer: '',
-          commentary: '',
+          questionSentence: '芽胞を形成する菌に有効でない滅菌・消毒法はどれか(複数選択可)',
+          answerImg: [disinfection],
+          choices: ["酸化エチレンガス滅菌", "アルコール", "高圧蒸気滅菌", "過酸化水素低温ガスプラズマ滅菌", "界面活性剤"],
+          answer: 'アルコール、界面活性剤',
+          commentary: '芽胞を形成する菌に有効なのは滅菌あり、酸化エチレンガス滅菌、高圧蒸気滅菌（オートクレーブ）、過酸化水素低温ガスプラズマ滅菌は大量の芽胞を含む、全ての微生物を殺滅あるいは除去することができる。アルコールは中水準消毒であり、芽胞を除く結核菌、その他の細菌、ほとんどのウイルスや真菌を殺滅させるが、芽胞を形成する菌には有効ではない。界面活性剤（石鹸）は低水準消毒であり、ほとんどの細菌や真菌、一部のウイルスを殺滅させるが、結核菌や芽胞は殺滅させず、芽胞を形成する菌には有効ではない。',
         },
         {
           detailInfo: '',
           questionImg: [],
-          questionSentence:
-            'どれか',
+          questionSentence: '次の文章について正しいのはどれか（複数選択可）',
           answerImg: [],
-          choices:["","","","",""],
-          answer: '',
-          commentary: '',
+          choices: ["北里柴三郎は1987年に赤痢菌の培養に成功し、赤痢菌の学名は北里の名前が由来である", "ジェンナーは、埃が外部から侵入しえない、白鳥の首型フラスコを用いて自然発生説の否定に成功する", "志賀潔は破傷風菌の純粋培養に成功し、ペスト菌を発見した", "感染症の病原体を特定する際の指針の一つとしてコッホの4原則が定められた", "中野竜一は、新型コロナウイルスを不活化する新規複合酸化物の開発に携わった。"],
+          answer: '4と5が正しい',
+          commentary: '志賀潔は赤痢菌の純培養に成功し、赤痢菌をShigellaと名付けた。これは病原菌の学名に日本人の名前がついている唯一の菌名である。パスツールは煮沸した肉汁を埃の入らない「白鳥の首型フラスコ」に入れておけば腐敗しないことを示し、当時信じられていた自然発生説を否定した。北里柴三郎は破傷風菌の純粋培養に成功し、ペスト菌を発見した。　コッホの4原則は、1.ある一定の病気には一定の微生物が見いだされること、2.その微生物を分離できること、3.分離した微生物を感受性のある動物に感染させて同じ病気を起こせること、4.そしてその病巣部から同じ微生物が分離されること　を定めた感染症の指針。',
         },
         {
           detailInfo: '',
           questionImg: [],
-          questionSentence:
-            'どれか',
-          answerImg: [],
-          choices:["","","","",""],
-          answer: '',
-          commentary: '',
+          questionSentence: 'グラム陰性菌とグラム陽性菌について大きな特徴の違いはどれか（複数選択可）',
+          answerImg: [GramMethod],
+          choices: ["ペプチドグリカン層の厚み", "リポ多糖体の有無", "鞭毛の数", "リン脂質の有無", "プラスミドの有無"],
+          answer: 'ペプチドグリカン層の厚み、リポ多糖体の有無、リン脂質の有無',
+          commentary: '厚いペプチドグリカン層がグラム染色によって染色される。リポ多糖体はLPSとも。例えばグラム陰性桿菌の大腸菌は鞭毛を持たないし、鞭毛の数がグラム染色で染め分けられているわけではない。プラスミドは染色体外の環状DNAのことで、陽性菌も陰性菌も両方持っている。',
         },
-        {
-          detailInfo: '',
-          questionImg: [],
-          questionSentence:
-            'どれか',
-          answerImg: [],
-          choices:["","","","",""],
-          answer: '',
-          commentary: '',
-        },
-        {
-          detailInfo: '',
-          questionImg: [],
-          questionSentence:
-            'どれか',
-          answerImg: [],
-          choices:["","","","",""],
-          answer: '',
-          commentary: '',
-        },
-        {
-          detailInfo: '',
-          questionImg: [],
-          questionSentence:
-            'どれか',
-          answerImg: [],
-          choices:["","","","",""],
-          answer: '',
-          commentary: '',
-        },
-        {
-          detailInfo: '',
-          questionImg: [],
-          questionSentence:
-            'どれか',
-          answerImg: [],
-          choices:["","","","",""],
-          answer: '',
-          commentary: '',
-        },
-        {
-          detailInfo: '',
-          questionImg: [],
-          questionSentence:
-            'どれか',
-          answerImg: [],
-          choices:["","","","",""],
-          answer: '',
-          commentary: '',
-        },
-        {
-          detailInfo: '',
-          questionImg: [],
-          questionSentence:
-            'どれか',
-          answerImg: [],
-          choices:["","","","",""],
-          answer: '',
-          commentary: '',
-        },
-        {
-          detailInfo: '',
-          questionImg: [],
-          questionSentence:
-            'どれか',
-          answerImg: [],
-          choices:["","","","",""],
-          answer: '',
-          commentary: '',
-        },
-                // {
+        // {
+        //   detailInfo: '',
+        //   questionImg: [],
+        //   questionSentence: '内毒素・外毒素のどれか',
+        //   answerImg: [],
+        //   choices: ["", "", "", "", ""],
+        //   answer: '',
+        //   commentary: '',
+        // },
+        // {
+        //   detailInfo: '',
+        //   questionImg: [],
+        //   questionSentence: 'どれか',
+        //   answerImg: [],
+        //   choices: ["", "", "", "", ""],
+        //   answer: '',
+        //   commentary: '',
+        // },
+        // {
+        //   detailInfo: '',
+        //   questionImg: [],
+        //   questionSentence: 'どれか',
+        //   answerImg: [],
+        //   choices: ["", "", "", "", ""],
+        //   answer: '',
+        //   commentary: '',
+        // },
+        // {
+        //   detailInfo: '',
+        //   questionImg: [],
+        //   questionSentence: 'どれか',
+        //   answerImg: [],
+        //   choices: ["", "", "", "", ""],
+        //   answer: '',
+        //   commentary: '',
+        // },
+        // {
+        //   detailInfo: '',
+        //   questionImg: [],
+        //   questionSentence: 'どれか',
+        //   answerImg: [],
+        //   choices: ["", "", "", "", ""],
+        //   answer: '',
+        //   commentary: '',
+        // },
+        // {
+        //   detailInfo: '',
+        //   questionImg: [],
+        //   questionSentence: 'どれか',
+        //   answerImg: [],
+        //   choices: ["", "", "", "", ""],
+        //   answer: '',
+        //   commentary: '',
+        // },
+        // {
+        //   detailInfo: '',
+        //   questionImg: [],
+        //   questionSentence: 'どれか',
+        //   answerImg: [],
+        //   choices: ["", "", "", "", ""],
+        //   answer: '',
+        //   commentary: '',
+        // },
+        // {
         //   detailInfo: '',
         //   questionImg: [],
         //   questionSentence:
@@ -220,7 +216,7 @@ export const useQuestionList = () => {
         //   answer: '',
         //   commentary: '',
         // },
-                // {
+        // {
         //   detailInfo: '',
         //   questionImg: [],
         //   questionSentence:
@@ -229,7 +225,7 @@ export const useQuestionList = () => {
         //   answer: '',
         //   commentary: '',
         // },
-                // {
+        // {
         //   detailInfo: '',
         //   questionImg: [],
         //   questionSentence:
@@ -238,7 +234,7 @@ export const useQuestionList = () => {
         //   answer: '',
         //   commentary: '',
         // },
-                // {
+        // {
         //   detailInfo: '',
         //   questionImg: [],
         //   questionSentence:
@@ -247,7 +243,7 @@ export const useQuestionList = () => {
         //   answer: '',
         //   commentary: '',
         // },
-                // {
+        // {
         //   detailInfo: '',
         //   questionImg: [],
         //   questionSentence:
@@ -256,7 +252,7 @@ export const useQuestionList = () => {
         //   answer: '',
         //   commentary: '',
         // },
-                // {
+        // {
         //   detailInfo: '',
         //   questionImg: [],
         //   questionSentence:
@@ -265,7 +261,7 @@ export const useQuestionList = () => {
         //   answer: '',
         //   commentary: '',
         // },
-                // {
+        // {
         //   detailInfo: '',
         //   questionImg: [],
         //   questionSentence:
@@ -274,7 +270,7 @@ export const useQuestionList = () => {
         //   answer: '',
         //   commentary: '',
         // },
-                // {
+        // {
         //   detailInfo: '',
         //   questionImg: [],
         //   questionSentence:
@@ -283,7 +279,7 @@ export const useQuestionList = () => {
         //   answer: '',
         //   commentary: '',
         // },
-                // {
+        // {
         //   detailInfo: '',
         //   questionImg: [],
         //   questionSentence:
@@ -292,7 +288,7 @@ export const useQuestionList = () => {
         //   answer: '',
         //   commentary: '',
         // },
-                // {
+        // {
         //   detailInfo: '',
         //   questionImg: [],
         //   questionSentence:
@@ -302,7 +298,145 @@ export const useQuestionList = () => {
         //   commentary: '',
         // },
       ]
-    }
+    }, {
+      groupTag: "2022本試験",
+      groupContents: [{
+          detailInfo: '',
+          questionImg: [],
+          questionSentence: '細菌について正しいのはどれか',
+          choices: ["細菌にはリボソームが存在しない", "結核菌は芽胞形成菌である", "プラスミドを接合伝達させるためには性腺毛を必要とする", "クラミジアにはグラム染色が有用である", "グラム染色の接合性の違いは莢膜による"],
+          answerImg: [bacteriaStructure, tuberculosis, cilia, Chlamydiae, GramMethod],
+          answer: 'プラスミドを接合伝達させるためには性線毛を必要とする',
+          commentary: '細菌はタンパク合成のためのリボソームやプラスミドを保有している。性腺毛を持っている細菌はプラスミドの接合伝達によってペニシリナーゼやESBL、MBL産生遺伝子を伝達し薬剤耐性を伝播することがある。結核菌は外側をミコール酸で覆われているその特徴から一般的な抗菌薬が無効であるため、抗結核薬が使用される。結核はその他鞭毛・莢膜を持たず、通常の染色法では染まりにくい特徴も持っている。クラミジアはグラム染色で染色されるペプチドグリカン層を持っていない。',
+        },
+        {
+          detailInfo: '',
+          questionImg: [],
+          questionSentence: '',
+          choices: ["", "", "", "", ""],
+          answerImg: [],
+          answer: '',
+          commentary: '',
+        },
+        {
+          detailInfo: '',
+          questionImg: [],
+          questionSentence: '',
+          choices: ["", "", "", "", ""],
+          answerImg: [],
+          answer: '',
+          commentary: '',
+        },
+        {
+          detailInfo: '',
+          questionImg: [],
+          questionSentence: '',
+          choices: ["", "", "", "", ""],
+          answerImg: [],
+          answer: '',
+          commentary: '',
+        },
+        {
+          detailInfo: '',
+          questionImg: [],
+          questionSentence: '',
+          choices: ["", "", "", "", ""],
+          answerImg: [],
+          answer: '',
+          commentary: '',
+        },
+        {
+          detailInfo: '',
+          questionImg: [],
+          questionSentence: '',
+          choices: ["", "", "", "", ""],
+          answerImg: [],
+          answer: '',
+          commentary: '',
+        },
+        {
+          detailInfo: '',
+          questionImg: [],
+          questionSentence: '',
+          choices: ["", "", "", "", ""],
+          answerImg: [],
+          answer: '',
+          commentary: '',
+        },
+        {
+          detailInfo: '',
+          questionImg: [],
+          questionSentence: '',
+          choices: ["", "", "", "", ""],
+          answerImg: [],
+          answer: '',
+          commentary: '',
+        },
+        {
+          detailInfo: '',
+          questionImg: [],
+          questionSentence: '',
+          choices: ["", "", "", "", ""],
+          answerImg: [],
+          answer: '',
+          commentary: '',
+        },
+        {
+          detailInfo: '',
+          questionImg: [],
+          questionSentence: '',
+          choices: ["", "", "", "", ""],
+          answerImg: [],
+          answer: '',
+          commentary: '',
+        },
+        {
+          detailInfo: '',
+          questionImg: [],
+          questionSentence: '',
+          choices: ["", "", "", "", ""],
+          answerImg: [],
+          answer: '',
+          commentary: '',
+        },
+        // {
+        //   detailInfo: '',
+        //   questionImg: [],
+        //   questionSentence:
+        //     '',
+        //   answerImg: [],
+        //   answer: '',
+        //   commentary: '',
+        // },
+        // {
+        //   detailInfo: '',
+        //   questionImg: [],
+        //   questionSentence:
+        //     '',
+        //   answerImg: [],
+        //   answer: '',
+        //   commentary: '',
+        // },
+        // {
+        //   detailInfo: '',
+        //   questionImg: [],
+        //   questionSentence:
+        //     '',
+        //   answerImg: [],
+        //   answer: '',
+        //   commentary: '',
+        // },
+        // {
+        //   detailInfo: '',
+        //   questionImg: [],
+        //   questionSentence:
+        //     '',
+        //   answerImg: [],
+        //   answer: '',
+        //   commentary: '',
+        // },
+      ],
+    },
   ])
   const showQuestionList = () => {
     return questionList
